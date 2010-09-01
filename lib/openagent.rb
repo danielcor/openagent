07531cd80c5e6195e6b63f729fb8bb9839e2888b
Christopher Whiteley <chris.whiteley@billboard.net>
2010-09-01 14:45:56 -0400

    modified:   lib/openagent.rb 	lib/openagent/activity.rb 	lib/openagent/agent.rb 	lib/openagent/messages/sif_ack.rb 	lib/openagent/messages/sif_message.rb 	lib/openagent/zone.rb

--- lib/openagent.rb
+++ lib/openagent.rb
@@ -1,8 +1,34 @@
-require 'uuid'
-require 'roxml'
 require "net/http"
 require "net/https"
 require "uri"
+
+begin 
+  require 'uuid'
+rescue LoadError
+  begin 
+    require 'rubygems'
+    require 'uuid'
+  rescue LoadError
+    abort <<-ERROR
+The 'uuid' library could not be loaded. If you have RubyGems installed
+you can install uuid by doing "gem install uuid".
+ERROR
+  end
+end
+  
+begin
+  require 'roxml'
+rescue LoadError
+  begin 
+    require 'rubygems'
+    require 'roxml'
+  rescue LoadError
+    abort <<-ERROR
+The 'roxml' library could not be loaded. If you have RubyGems installed
+you can install ROXML by doing "gem install roxml".
+ERROR
+  end
+end
 
 
 require 'openagent/messages'
